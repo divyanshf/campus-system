952b0b1ec78d6036678ff86789ebba3649f5b096
Yuvaan1182 <hw9044@gmail.com>
2021-02-25 19:04:19 +0530

    //added custom test navigation drawer //mainscreen.xml //layout to drawer layout //added toolbar //mainscreen.kt //added toggler for navigation drawer

--- app/src/main/java/com/example/firebase/mainscreen.kt
+++ app/src/main/java/com/example/firebase/mainscreen.kt
@@ -2,6 +2,10 @@ package com.example.firebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -10,10 +14,19 @@ class mainscreen : AppCompatActivity() {
 
     lateinit var homeFragment: HomeFragment
     lateinit var chatroomFragment: ChatroomFragment
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mainscreen)
+
+        //setting up action bar
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val toolbar: Toolbar = findViewById(R.id.my_toolbar)
+        setSupportActionBar(toolbar)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
         val bottomNavigation : BottomNavigationView = findViewById(R.id.bottomnav)
 
@@ -43,9 +56,8 @@ class mainscreen : AppCompatActivity() {
 
         }
 
-
-
     }
+
 
     
 }
