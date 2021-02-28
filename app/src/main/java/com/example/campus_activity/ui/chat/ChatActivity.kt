e1d7a02a37ae317268b2fec2ded7f508c7efb834
Divyansh Falodiya <divyanshfalodiyaab@gmai.com>
2021-03-01 00:22:23 +0530

    upgraded the chat ui

--- app/src/main/java/com/example/campus_activity/ui/chat/ChatActivity.kt
+++ app/src/main/java/com/example/campus_activity/ui/chat/ChatActivity.kt
@@ -1,23 +1,32 @@
 package com.example.campus_activity.ui.chat
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.campus_activity.R
-import com.google.android.material.bottomappbar.BottomAppBar
 
 class ChatActivity : AppCompatActivity() {
     private lateinit var toolbar:Toolbar
-    private lateinit var bottomAppBar: BottomAppBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
         toolbar = findViewById(R.id.chat_toolbar)
-        bottomAppBar = findViewById(R.id.chat_bottom_app_bar)
 
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            android.R.id.home -> {
+                Toast.makeText(this, "Back to home", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> false
+        }
     }
 }
