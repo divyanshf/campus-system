9cbc3b9785c7ce2d42c21afe71eb632889083885
Prateek Singh <prateeksingh09apr@gmail.com>
2021-03-09 23:15:02 +0530

    firestore new club

--- app/src/main/java/com/example/campus_activity/ui/chat/newClub.kt
+++ app/src/main/java/com/example/campus_activity/ui/chat/newClub.kt
@@ -1,12 +1,35 @@
 package com.example.campus_activity.ui.chat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import com.example.campus_activity.R
+import com.example.campus_activity.ui.main.MainActivity
+import com.example.campus_activity.ui.main.RoomListFragment
 
 class newClub : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_club)
+
+
+
+
+        val clubEdt : EditText= findViewById(R.id.club_Name)
+        val adminEdt : EditText = findViewById(R.id.admin_Roll)
+        val btnAdd: Button = findViewById(R.id.add_New_Club)
+
+
+
+
+        btnAdd.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
+
