850e5987ac748bab2145cad2d19d97a5c39d9626
Divyansh Falodiya <divyanshfalodiyaab@gmai.com>
2021-03-09 18:05:26 +0530

    added desc order

--- app/src/main/java/com/example/campus_activity/data/repository/FeedsRepository.kt
+++ app/src/main/java/com/example/campus_activity/data/repository/FeedsRepository.kt
@@ -1,19 +1,15 @@
 package com.example.campus_activity.data.repository
 
-import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.example.campus_activity.data.model.FeedModel
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
-import javax.inject.Inject
 
 class FeedsRepository(
     val firebaseFirestore: FirebaseFirestore
@@ -29,7 +25,7 @@ class FeedsRepository(
         CoroutineScope(Dispatchers.IO)
             .launch {
                 val snaps = firebaseFirestore.collection("feeds")
-                    .orderBy("timestamp")
+                    .orderBy("timestamp", Query.Direction.DESCENDING)
                     .get()
                     .await()
 
@@ -45,7 +41,6 @@ class FeedsRepository(
 
     private fun getFeed(snap:DocumentSnapshot) : FeedModel{
         return FeedModel(
-            snap.id,
             snap["sender_name"] as String,
             snap["posted_by"] as String,
             snap["post"] as String,
